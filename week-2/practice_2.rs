1aba62a6ad98290bd99036ff4d4073fa8f2a3cbe
Eregha Tarilah <tari4divine@gmail.com>
2025-10-08 17:17:14 +0100

    (wk 2 commit)

---
+++ week-2/practice_2.rs
@@ -0,0 +1,6 @@
+fn main(){
+	println!();//prints just a
+	println!("hello");//prints
+	println!("format {} arguments", "some");//prints format some argument
+	println!("My name is {}. i am the class rep of stream 1","Dagogo william-Jumbo");
+}
