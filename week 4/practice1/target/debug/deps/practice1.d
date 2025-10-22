a57f2cd444d1156210fa10d50fa35f7c73e0a4fa
Eregha Tarilah <tari4divine@gmail.com>
2025-10-22 16:23:54 +0100

    practice1

---
+++ week 4/practice1/target/debug/deps/practice1.d
@@ -0,0 +1,5 @@
+C:\Users\tari4\t.ereghaCOS101\week 4\practice1\target\debug\deps\practice1.d: src\main.rs
+
+C:\Users\tari4\t.ereghaCOS101\week 4\practice1\target\debug\deps\practice1.exe: src\main.rs
+
+src\main.rs:
