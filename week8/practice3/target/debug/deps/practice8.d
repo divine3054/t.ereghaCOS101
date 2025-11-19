615ade38d6198eb47cd8cb5fdcef54c012c5b6a6
Eregha Tarilah <tari4divine@gmail.com>
2025-11-19 16:46:51 +0100

    practice3

---
+++ week8/practice3/target/debug/deps/practice8.d
@@ -0,0 +1,5 @@
+C:\Users\tari4\t.ereghaCOS101\week8\practice3\target\debug\deps\practice8.d: src\main.rs
+
+C:\Users\tari4\t.ereghaCOS101\week8\practice3\target\debug\deps\practice8.exe: src\main.rs
+
+src\main.rs:
---
+++ week8/practice3/target/debug/practice8.d
@@ -0,0 +1,1 @@
+C:\Users\tari4\t.ereghaCOS101\week8\practice3\target\debug\practice8.exe: C:\Users\tari4\t.ereghaCOS101\week8\practice3\src\main.rs
