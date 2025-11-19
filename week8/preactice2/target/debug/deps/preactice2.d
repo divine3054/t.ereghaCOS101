1cb7cbd358a4fe601c269307b84102a75b10c5d6
Eregha Tarilah <tari4divine@gmail.com>
2025-11-19 16:30:51 +0100

    practice2

---
+++ week8/preactice2/target/debug/deps/preactice2.d
@@ -0,0 +1,5 @@
+C:\Users\tari4\t.ereghaCOS101\week8\preactice2\target\debug\deps\preactice2.d: src\main.rs
+
+C:\Users\tari4\t.ereghaCOS101\week8\preactice2\target\debug\deps\preactice2.exe: src\main.rs
+
+src\main.rs:
---
+++ week8/preactice2/target/debug/preactice2.d
@@ -0,0 +1,1 @@
+C:\Users\tari4\t.ereghaCOS101\week8\preactice2\target\debug\preactice2.exe: C:\Users\tari4\t.ereghaCOS101\week8\preactice2\src\main.rs
