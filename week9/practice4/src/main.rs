886460b8f160711b40cf59bad50654ce7850a260
Eregha Tarilah <tari4divine@gmail.com>
2025-11-26 16:05:38 +0100

    practice4

---
+++ week9/practice4/src/main.rs
@@ -0,0 +1,12 @@
+use std::fs::OpenOptions;
+use std::io::Write;
+fn main() {
+    let mut file = OpenOptions::new().append(true).open("../files/data.txt").expect("cannot open file");
+    let dept = "Department of Computer Sciences";
+
+    
+    file.write_all("\nHello class".as_bytes()).expect("Write Failed");
+    file.write_all("\nThis is the appendage to the document".as_bytes()).expect("Write failed");
+    
+    println!("file append success")
+}
